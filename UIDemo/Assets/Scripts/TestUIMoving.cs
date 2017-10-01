4ec2f2d1b1489418b0331a734f56c7d18e2ee35f
j5ngw <jw.kiru@gmail.com>
2017-10-01 21:59:13 +0900

    - UI 객체 이동 테스트코드

---
+++ UIDemo/Assets/Scripts/TestUIMoving.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUIMoving : MonoBehaviour
+{
+    public GameObject coinPrefab = null;
+    public Transform from = null;
+    public Transform target = null;
+
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            GameObject coin = Instantiate(coinPrefab) as GameObject;
+            coin.transform.SetParent(target.parent, false);
+            //coin.transform.localScale = Vector3.one;
+
+            //coin.transform.position = Input.mousePosition;
+
+            //coin.transform.position = from.position;
+            coin.GetComponent<RectTransform>().position = from.GetComponent<RectTransform>().position;
+
+            LeanTween.move(coin, target.position, 1.0f).setEase(LeanTweenType.easeInExpo).setOnComplete(()=> 
+            {
+                Destroy(coin);
+            });
+        }
+	}
+}
