817e1e58d401ee0fcbf6ccf42e7530dba97c0723
j5ngw <jw.kiru@gmail.com>
2017-10-01 21:36:17 +0900

    - UIDemo 프로젝트 추가 - 해당 프로젝트에 WidgetManager 에셋 추가

---
+++ UIDemo/Assets/Widgets/Source Code/Common/WidgetName.cs
@@ -0,0 +1,17 @@
+﻿
+namespace eeGames.Widget
+{
+    /// <summary>
+    /// Here Add your Widget ID's
+    /// </summary>
+    public enum WidgetName
+    {
+        MainMenu,
+        Setting,
+        AudioSetting,
+        GameSetting,
+        SaveSetting,
+        MiniGame
+    }
+
+}
